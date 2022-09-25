21629499b8419bf6977cec19a81a2336b17240b7
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-09-25 17:25:00 +0300

    Update 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -28,8 +28,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	}
 	else
 	{
-		parent = parent->left;
-		parent->left = new_node;
+		new_node->parent->left = new_node;
 		new_node->parent = parent;
 	}
 	new_node->left = NULL;
