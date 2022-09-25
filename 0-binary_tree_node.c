fb8f22baace9e983a24497ae79ac58c922803da0
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-09-25 15:42:59 +0300

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -16,10 +16,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	{
 		return (NULL);
 	}
-	if (!parent)
-	{
-		return (NULL);
-	}
 	new_node->n = value;
 	new_node->parent = parent;
 	new_node->left = NULL;
