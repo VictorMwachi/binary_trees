81932440ee476afcdb8db48c0541f38bd1eb6713
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-09-25 15:46:36 +0300

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_insert_left - inserts node as the left child of anode
+ * binary_tree_node - icreates a binary tree node
  * @parent:  is a pointer to the node to insert the left-child in
  * @value: is the value to store in the new node
  *
