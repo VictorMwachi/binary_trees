b13053b7bd1b28dd85c360801d2cc948657bc2fe
root <mudakizevictor@gmail.com>
2022-09-25 05:31:32 -0700

    0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,7 +7,7 @@
  * Return: must return a pointer to the created node,
  * or NULL on failure or if parent is NUL
  */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
