b327f43769c39ab914b134ecd4b9628f92bc7478
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-09-27 15:16:19 +0300

    Update 4-binary_tree_is_leaf.c

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -14,11 +14,11 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 	}
 	if(node->left == NULL && node->right == NULL)
 	{
-		return (0);
+		return (1);
 	}
 	else
 	{
-		return (1);
+		return (0);
 	}
 	
 }
