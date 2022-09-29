2ca1240f5e7e63450bce5a7e4b10f736be9260b5
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-09-29 16:13:40 +0300

    Create 9-binary_tree_height.c

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_height -measures height of tree
+ *@tree: pointer to the root node
+ *Return: height of tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+  if
