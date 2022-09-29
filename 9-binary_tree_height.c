0c31fb36f34f7a117090b7f56f2e846293ce9edb
root <mudakizevictor@gmail.com>
2022-09-29 06:37:05 -0700

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,4 +7,25 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-  if
+	size_t height_right_branch;
+	size_t height_left_branch;
+
+	if (!tree)
+	{
+		return (0);
+	}
+	if (tree->left == NULL && tree->right)
+	{
+		return (0);
+	}
+	height_left_branch = binary_tree_height(tree->left) + 1;
+	height_right_branch = binary_tree_height(tree->right) + 1;
+	if (height_left_branch > height_right_branch)
+	{
+		return (height_left_branch);
+	}
+	else
+	{
+		return (height_right_branch);
+	}
+}
