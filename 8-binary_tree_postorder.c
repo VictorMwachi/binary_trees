c47935663110b06eea7e49fcb237efec10003f2e
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-09-29 15:32:00 +0300

    Update 8-binary_tree_postorder.c

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -6,4 +6,12 @@
  * The value in the node must be passed as a parameter to this function.
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
-{}
+{
+  if (!tree || !func)
+	{
+		return;
+	}
+  func(tree->n);
+  binary_tree_postorder(tree->right, func);
+  binary_tree_postorder(tree->left, func);
+}
