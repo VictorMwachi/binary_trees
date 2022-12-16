f9049ab05b1ce4db59d95820093f0598d98180e1
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2022-12-16 11:39:21 +0300

    depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,31 +1,14 @@
 #include "binary_trees.h"
 
 /**
- *binary_tree_depth -measures depth of tree
+ *binary_tree_depth - measures the depth of a node in a binary tree
  *@tree: pointer to the root node
  *Return: depth of tree
  */
+
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t depth_right_branch;
-	size_t depth_left_branch;
-
-	if (!tree)
-	{
+	if (tree == NULL || tree->parent == NULL)
 		return (0);
-	}
-	if (tree->left == NULL && tree->right)
-	{
-		return (0);
-	}
-	depth_left_branch = binary_tree_depth(tree->left) + 1;
-	depth_right_branch = binary_tree_depth(tree->right) + 1;
-	if (height_left_branch > height_right_branch)
-	{
-		return (depth_left_branch);
-	}
-	else
-	{
-		return (depth_right_branch);
-	}
+	return (binary_tree_depth(tree->parent) + 1);
 }
